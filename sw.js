dcff423315ca15b89e80da73903a67f35f81e8ec
carlessp <carles.hm@gmail.com>
2025-01-21 20:58:30 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'laberint-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/imatges/d/amanida.png',
+  '/imatges/d/banyador.png',
+  // Afegeix aquí totes les rutes dels arxius que vulguis emmagatzemar en memòria cau
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
